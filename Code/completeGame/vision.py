43a8700871510246899c485dce618798b5ee0a59
AldaFux <aldafux@gmail.com>
2016-12-05 16:39:02 -0800

    Vision update
    -Added functionality to detect sign next to field
    -Added functionality to supress detection of line in field

--- Code/completeGame/vision.py
+++ Code/completeGame/vision.py
@@ -9,6 +9,7 @@ class Image():
         self.transform = []
         self.gray = []
         self.adap_thresh = []
+        self.left_region = np.zeros([2,2])
         self.width = 0
         self.height = 0
         self.sidelength = 0
@@ -37,6 +38,25 @@ class Image():
                 self.sidelength = self.height
             else:
                 self.sidelength = self.width
+        
+    def __is_moving(self,img):
+        # http://docs.opencv.org/3.0-beta/doc/py_tutorials/py_imgproc/py_morphological_ops/py_morphological_ops.html
+        kernel = np.ones((5,5),np.uint8)
+        fgmask = self.fgbg.apply(img)
+        ret, bw = cv2.threshold(fgmask, 127, 255, cv2.THRESH_BINARY)
+        opening = cv2.morphologyEx(bw, cv2.MORPH_OPEN, kernel)
+        
+        opening, contours, hierarchy = cv2.findContours(opening,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+        
+        # loop over the contours
+        for cnt in contours:
+            # if the contour is too small, ignore it
+            if cv2.contourArea(cnt) < self.moving_min_area:
+                continue
+            else:
+                return True
+        print('No movement detected.')
+        return False
             
     def show_frame(self):
         cv2.imshow('Frame captured',self.orig)
@@ -47,10 +67,7 @@ class Image():
         cv2.imshow('Transformed board',self.transform)
         cv2.waitKey(0)
         cv2.destroyAllWindows
-            
-    # def closeAll(self):
-        # cv2.destroyAllWindows()
-            
+                        
     def calibrate(self):
         # Record two images, so that the camera adjusts shutter time
         self.__record_frame()
@@ -66,6 +83,7 @@ class Image():
         
         # Find inner contour which should be the middle field
         highest_hierarchy_level = -1
+        found_middle_field = False
         for j in range(0,len(contours)):
             cnt = contours[j]
             hull = cv2.convexHull(cnt)
@@ -78,11 +96,22 @@ class Image():
             if hier[3]>highest_hierarchy_level:
                 highest_hierarchy_level = hier[3]
                 inner_cnt = j
+                found_middle_field = True
         
+        if found_middle_field==False:
+            raise Exception ('Was not able to detect the field.')
         # Fit a rectangle in the detected middle field
         cnt = contours[inner_cnt]
         x,y,w,h = cv2.boundingRect(cnt)
-    
+        # Save the lower right corner of the region left from the field
+        # Here, the sign indicating if the player wants to start or not will be detected
+        left_region_x = x-1.5*w
+        left_region_y = y+2.5*h
+        if (left_region_x<0):
+            raise Exception ('Not able to see region left of the field. Adjust camera position.')
+        if (left_region_y>self.height):
+            left_region_y = self.height
+        self.left_region[1,:] = [left_region_x,left_region_y]
         # Calculate corner locations in source image from rectangle corners
         urSrc = [x+w,y]
         lrSrc = [x+w,y+h]
@@ -102,56 +131,39 @@ class Image():
         self.transform = cv2.warpPerspective(self.orig,self.transformationMatrix,(self.sidelength,self.sidelength))
         self.calibrated = True
         
-    def is_moving(self):
-        self.__record_frame()
-        # http://docs.opencv.org/3.0-beta/doc/py_tutorials/py_imgproc/py_morphological_ops/py_morphological_ops.html
-        kernel = np.ones((5,5),np.uint8)
-        fgmask = self.fgbg.apply(self.transform)
-        ret, bw = cv2.threshold(fgmask, 127, 255, cv2.THRESH_BINARY)
-        opening = cv2.morphologyEx(bw, cv2.MORPH_OPEN, kernel)
-        
-        opening, contours, hierarchy = cv2.findContours(opening,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-        
-        # loop over the contours
-        for cnt in contours:
-            # if the contour is too small, ignore it
-            if cv2.contourArea(cnt) < self.moving_min_area:
-                continue
-            else:
-                return True
-        print('No movement detected.')
-        return False
-        
     def detect_sign(self,emptyTiles):
         # emptyTiles should list with empty tiles
         emptyTiles_shift = list()
         emptyTiles_shift = [x-1 for x in emptyTiles]
         
-        print ( emptyTiles)
+        # print (emptyTiles)
         
         # shape detection from
         # http://stackoverflow.com/questions/31974843/detecting-lines-and-shapes-in-opencv-using-python
         # and http://docs.opencv.org/trunk/dd/d49/tutorial_py_contour_features.html
         
-        while self.is_moving():
+        self.__record_frame()
+        
+        while self.__is_moving(self.transform):
             cv2.imshow('Transformed board',self.transform)
             print("Something is moving.")
             cv2.waitKey(500)
-        self.__record_frame()
+        
         cv2.imshow('Transformed board',self.transform)
 
-        # RETR_EXTERNAL
-        # If you use this flag, it returns only extreme outer flags. All child contours are left behind. 
-        # http://docs.opencv.org/trunk/d9/d8b/tutorial_py_contours_hierarchy.html
-        y11 = x11 = 0
-        y12 = x12 = int(0.3*self.sidelength)
-        y21 = x21 = int(0.4*self.sidelength)
-        y22 = x22 = int(0.6*self.sidelength)
-        y31 = x31 = int(0.7*self.sidelength)
-        y32 = x32 = int(self.sidelength)
-        
         gray = cv2.cvtColor(self.transform,cv2.COLOR_BGR2GRAY)
         bw = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 123, 50)
+        
+        # res = self.transform.copy() # DEBUG
+        
+        # Define the regions of the tiles in the image
+        y11 = x11 = 0
+        y12 = x12 = int(0.35*self.sidelength)
+        y21 = x21 = int(0.35*self.sidelength)
+        y22 = x22 = int(0.65*self.sidelength)
+        y31 = x31 = int(0.65*self.sidelength)
+        y32 = x32 = int(self.sidelength)
+        # Create a Region of Interest for each tile
         tile_ll = bw[y31:y32,x11:x12]
         tile_lm = bw[y31:y32,x21:x22]
         tile_lr = bw[y31:y32,x31:x32]
@@ -164,12 +176,37 @@ class Image():
         tiles = [tile_ll,tile_lm,tile_lr,tile_ml,tile_mm,tile_mr,tile_ul,tile_um,tile_ur]
         # print('The length of the list of tiles is {}'.format(len(tiles)))
         # print('The length of the list of tiles[0] is {}'.format(len(tiles[0])))
-        res = self.transform.copy()
+        
+        
+        # Define the regions where the center of the token needs to be
+        yc11 = xc11 = 0
+        yc12 = xc12 = int(0.25*self.sidelength)
+        yc21 = xc21 = int(0.45*self.sidelength)
+        yc22 = xc22 = int(0.55*self.sidelength)
+        yc31 = xc31 = int(0.75*self.sidelength)
+        yc32 = xc32 = int(self.sidelength)
+        # Create lower and upper bounds for x and y values for each tile
+        center_region_ll = np.array([[xc11,yc31],[xc12,yc32]])
+        center_region_lm = np.array([[xc21,yc31],[xc22,yc32]])
+        center_region_lr = np.array([[xc31,yc31],[xc32,yc32]])
+        center_region_ml = np.array([[xc11,yc21],[xc12,yc22]])
+        center_region_mm = np.array([[xc21,yc21],[xc22,yc22]])
+        center_region_mr = np.array([[xc31,yc21],[xc32,yc22]])
+        center_region_ul = np.array([[xc11,yc11],[xc12,yc12]])
+        center_region_um = np.array([[xc21,yc11],[xc22,yc12]])
+        center_region_ur = np.array([[xc31,yc11],[xc32,yc12]])
+        center_regions = np.array([center_region_ll,center_region_lm,center_region_lr,center_region_ml,center_region_mm,center_region_mr,center_region_ul,center_region_um,center_region_ur])
+        # Indexing: center_regions[tile,bound upper/lower,x/y]
+        
         for i in emptyTiles_shift:
             tile = tiles[i]
             print('Looking for token in tile {}'.format(i+1))
             # cv2.imshow('tile',tile)
             # print('The sidelength of one tile is {}'.format(tile.shape))
+            
+            # RETR_EXTERNAL
+            # If you use this flag, it returns only extreme outer flags. All child contours are left behind. 
+            # http://docs.opencv.org/trunk/d9/d8b/tutorial_py_contours_hierarchy.html
             cnt_img, contours, hierarchy = cv2.findContours(tile,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
             
             tokens = list(contours)
@@ -179,12 +216,19 @@ class Image():
                 hull_indices = cv2.convexHull(cnt,returnPoints = False)
                 hull = cv2.convexHull(cnt)
                 area = cv2.contourArea(hull)
-                
+                # Check if area covered by contour is big enough to be a token
                 if (area<self.token_min_area):
                     tokens.pop(j-deleteCounter)
                     deleteCounter += 1
                     continue
-                
+                # Check if center of mass of the area is in the center region of the tile
+                M = cv2.moments(cnt)
+                cx = int(M['m10']/M['m00'])
+                cy = int(M['m01']/M['m00'])
+                if not ((center_regions[i,0,0] < cx) and (cx < center_regions[i,1,0]) and (center_regions[i,0,1] < cy) and (cy < center_regions[i,1,1])):
+                    # Center of mass of the contour is NOT in middle region of the tile
+                    continue
+                    
                 # cv2.drawContours(tile,cnt,-1,(255,0,0),3)
                 defects = cv2.convexityDefects(cnt, hull_indices)
                 distances = np.array(defects[:,:,3])
@@ -216,115 +260,66 @@ class Image():
                     print('Found circle in tile {}'.format(i+1))
                     return [True,'O', i+1]
                     
-            # cv2.imshow('tile',tile)
-            # cv2.waitKey(0)
-            # cv2.destroyAllWindows
-        # cv2.imshow('found',res)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows
-        
         cv2.waitKey(500)
         return [False,'.',-1]
         
-class CoordinateStore():
-    def __init__(self):
-        self.points = []
-        self.done = False
-        self._cropping = False
-    
-    def setImage(self,image):
-        self.original = image.copy()
-        self.result = image.copy()
-    
-    def recordCoordinates(self):
-        cv2.imshow('Select Middle Field',self.result)
-        cv2.setMouseCallback('Select Middle Field',self.select_points)
-        cv2.waitKey(0)
-        if not self.points:
-            raise Exception("ERROR: Coordinate capturing did not conclude successfully.")
-        elif len(self.points[0]) == 2:
-            print('Coordinates for ROI successfully recorded.')
-        cv2.destroyAllWindows()
-        return [self.getX0(),self.getY0(),self.getX1(),self.getY1()]
+    def detect_first_move(self):
+        if not self.calibrated:
+            raise Exception ('Calibrate the camera before detecting the first move.')
+        self.__record_frame()
+        roi = self.orig[self.left_regi`on[0,1]:self.left_region[1,1],self.left_region[0,0]:self.left_region[1,0]]
         
-    def getX0(self):
-        # return left x value
-        if self.points[0][0]<self.points[1][0]:
-            return self.points[0][0]
-        else:
-            return self.points[1][0]
+        while self.__is_moving(roi):
+            print("Something is moving.")
+            cv2.waitKey(500)
         
-    def getX1(self):
-        # return right x value
-        if self.points[0][0]<self.points[1][0]:
-            return self.points[1][0]
-        else:
-            return self.points[0][0]
+        gray = cv2.cvtColor(roi,cv2.COLOR_BGR2GRAY)
+        bw = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 123, 50)
         
-    def getY0(self):
-        # return top y value
-        if self.points[0][1]<self.points[1][1]:
-            return self.points[0][1]
-        else:
-            return self.points[1][1]
-    
-    def getY1(self):
-        # return bottom value
-        if self.points[0][1]<self.points[1][1]:
-            return self.points[1][1]
-        else:
-            return self.points[0][1]
+            
+        # RETR_EXTERNAL
+        # If you use this flag, it returns only extreme outer flags. All child contours are left behind. 
+        # http://docs.opencv.org/trunk/d9/d8b/tutorial_py_contours_hierarchy.html
+        cnt_img, contours, hierarchy = cv2.findContours(bw,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
         
-    # def select_points(self,event,x,y,flags,param):                
-            # if event == cv2.EVENT_LBUTTONDOWN:
-                # if not self._cropping:
-                    # self.points = [(x,y)]
-                    # self._cropping = True
-                # else:
-                    # self.points.append((x,y))
-                    # if len(self.points)==4:
-                        # self.result = self.original.copy()
-                        # for i in range(0,len(self.points)-1):
-                            # cv2.line(self.result, self.points[i], self.points[i+1], (0, 255, 0), 1)
-                        # cv2.line(self.result, self.points[3], self.points[0], (0, 255, 0), 1)    
-                        # cv2.imshow('Select Middle Field',self.result)
-                        # self.done = True
-                        # self._cropping = False
+        for j in range(0,len(contours)):
+            cnt = contours[j]
+            hull_indices = cv2.convexHull(cnt,returnPoints = False)
+            hull = cv2.convexHull(cnt)
+            area = cv2.contourArea(hull)
+            # Check if area covered by contour is big enough to be a token
+            if (area<self.tile_min_area/2):
+                continue
                 
-            # if event == cv2.EVENT_MOUSEMOVE and self._cropping:
-                # self.result = self.original.copy()
-                # for i in range(0,len(self.points)-1):
-                    # cv2.line(self.result, self.points[i], self.points[i+1], (0, 255, 0), 1)
-                # cv2.line(self.result, self.points[len(self.points)-1], (x,y), (0, 255, 0), 1)    
-                # cv2.imshow('Select Middle Field',self.result)
-                
-            # if event == cv2.EVENT_RBUTTONDOWN:
-                # self.points = []
-                # self.done = False
-                # self._cropping = False
-                # self.result = self.original.copy()
-                # cv2.imshow('Select Middle Field',self.result)
-                    
-                    
-                    
-    def select_points(self,event,x,y,flags,param):                
-        if event == cv2.EVENT_LBUTTONDOWN:
-            if not self._cropping:
-                self.points = [(x,y)]
-                self._cropping = True
+            # cv2.drawContours(tile,cnt,-1,(255,0,0),3)
+            defects = cv2.convexityDefects(cnt, hull_indices)
+            distances = np.array(defects[:,:,3])
+            # print('The distances in tile {} are:'.format(i))
+            # print(distances)
+            # print('The area of the token in tile {} is {}'.format(i,area))
+            mean = np.mean(distances[1:])
+            # xOffset = int(((0.4*(i%3>0)) + (0.3*(i%3>1)))*self.sidelength)
+            # yOffset = int(((0.4*((i//3)<2)) + (0.3*((i//3)<1)))*self.sidelength)
+            # offset = (xOffset,yOffset)
+            if 800<mean:
+                # CROSS
+                # x,y,w,h = cv2.boundingRect(cnt)
+                # cv2.line(res,(x+xOffset,y+yOffset),(x+w+xOffset,y+h+yOffset),(60,200,255),2)
+                # cv2.line(res,(x+xOffset,y+h+yOffset),(x+w+xOffset,y+yOffset),(60,200,255),2)
+                # cv2.line(tile,(x,y),(x+w,y+h),(60,200,255),2)
+                # cv2.line(tile,(x,y+h),(x+w,y),(60,200,255),2)
+                print('Found cross.')
+                return [True,'X']
             else:
-                self.points.append((x,y))
-                self.done = True
-                self._cropping = False
-            
-        if event == cv2.EVENT_MOUSEMOVE and self._cropping:
-            self.result = self.original.copy()
-            cv2.rectangle(self.result, self.points[0], (x,y), (0, 255, 0), 1)
-            cv2.imshow('Select Middle Field',self.result)
-            
-        if event == cv2.EVENT_RBUTTONDOWN:
-            self.points = []
-            self.done = False
-            self._cropping = False
-            self.result = self.original.copy()
-            cv2.imshow('Select Middle Field',self.result)
+                # CIRCLE
+                # (x,y),radius = cv2.minEnclosingCircle(cnt)
+                # center = (int(x+xOffset),int(y+yOffset))
+                # center2 = (int(x),int(y))
+                # radius = int(radius)
+                # cv2.circle(res,center,radius,(200,60,255),2)
+                # cv2.circle(tile,center2,radius,(200,60,255),2)
+                print('Found circle.')
+                return [True,'O']
+                
+        cv2.waitKey(500)
+        return [False,'.']
